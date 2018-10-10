87cd0035dd8fde2d97ed842c9150cf7dd8682b93
winton <mail@wintoni.us>
2018-10-10 15:12:54 -0400

    Upgrade dot-event and dot-store

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,8 +1,16 @@
+import dotEvent from "dot-event"
 import dotStore from "dot-store"
 import glob from "../dist/glob"
 
 test("glob files", async () => {
-  const store = glob(dotStore())
-  await store.glob("test", `${__dirname}/*Test.js`)
-  expect(store.get("test").length).toBe(1)
+  const events = dotEvent()
+  const store = dotStore(events)
+
+  glob({ events, store })
+
+  await events.glob("test", {
+    pattern: `${__dirname}/*Test.js`,
+  })
+
+  expect(store.get("glob.test").length).toBe(1)
 })
